647a576c02b3029cfca2ac959a7ccbe29af24cc5
dervinaltenor1 <156249938+dervinaltenor1@users.noreply.github.com>
2024-06-24 21:25:03 -0400

    Update AddTodo.test.js

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -16,20 +16,7 @@ afterEach(() => {
   container = null;
 });
 
-test('test that App component renders Task', () => {
-    render(<App />);
-    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
-    const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
-    const element = screen.getByRole('button', {name: /Add/i});
-    const dueDate = "05/30/2023";
-    fireEvent.change(inputTask, { target: { value: "History Test"}});
-    fireEvent.change(inputDate, { target: { value: dueDate}});
-    fireEvent.click(element);
-    const check = screen.getByText(/History Test/i);
-    const checkDate = screen.getByText(new RegExp(dueDate, "i"));
-    expect(check).toBeInTheDocument();
-    expect(checkDate).toBeInTheDocument();
-});
+
 
 test('test that App component doesn\'t add a task without task name', () => {
     render(<App />);
